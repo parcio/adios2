6daf5986b9032bfadacbab04a8f65a20acff8c19
Kira <bella_25.1@hotmail.com>
2019-08-09 14:18:58 +0200

    cleaning Interaction Reader

--- source/adios2/engine/julea-kv/JuleaInteractionReader.cpp
+++ source/adios2/engine/julea-kv/JuleaInteractionReader.cpp
@@ -89,65 +89,7 @@ ADIOS2_FOREACH_STDTYPE_1ARG(variable_template_instantiation)
 /** -------------------------------------------------------------------------**/
 /** -------------- TESTING GENERIC FUNCTIONS --------------------------------**/
 /** -------------------------------------------------------------------------**/
-// template <class T>
-// void GetAllVariableNamesFromJulea(Variable<T> &variable, bson_t
-// *bsonMetadata,
-//                                  std::string nameSpace)
 
-// void ExtractVariableFromBSON(const std::string nameSpace, const std::string
-// varName, bson_t *bsonMetadata, int type, Dims shape, Dims start, Dims
-// count,bool constantDims)
-// {
-
-// }
-
-void GetVariableBSONFromJulea(const std::string nameSpace, const std::string
-varName, bson_t *bsonMetadata)
-{
-	// JBatch *batch;
-    // gchar *string_metadata_kv;
-    // gchar *key;
-    // bson_t* bson_metadata;
-    // bson_t bson_metadata;
-    bson_iter_t b_iter;
-    guint32 value_len = 0;
-
-    // g_autoptr(JKV) kv_object = NULL;
-    void *meta_data_buf = NULL;
-    auto semantics = j_semantics_new(J_SEMANTICS_TEMPLATE_DEFAULT);
-    auto batch = j_batch_new(semantics);
-
-    auto string_metadata_kv = g_strdup_printf("variables_%s",
-    nameSpace.c_str()); auto kv_object = j_kv_new(string_metadata_kv,
-    varName.c_str());
-    // bson_metadata = bson_new();
-
-    j_kv_get(kv_object, &meta_data_buf, &value_len, batch);
-    j_batch_execute(batch);
-
-    if (value_len == 0)
-    {
-        // bson_names = bson_new();
-        printf("WARNING: The variable key-value store is empty! \n");
-    }
-    else
-    {
-        bson_init_static(bsonMetadata, (uint8_t *)meta_data_buf, value_len);
-    }
-
-    // bson_iter_init(&b_iter, bson_metadata);
-    // if(bson_iter_init(&b_iter, bson_metadata))
-    if (bson_iter_init(&b_iter, bsonMetadata))
-    {
-        std::cout << "++ Julea Client Logic: Bson iterator is valid"
-                  << std::endl;
-    }
-    else
-    {
-        std::cout << "ERROR: Bson iterator is not valid!" << std::endl;
-    }
-
-}
 
 void GetNamesBSONFromJulea(const std::string nameSpace, bson_t **bsonNames,
                            unsigned int *varCount)
@@ -188,27 +130,6 @@ void GetNamesBSONFromJulea(const std::string nameSpace, bson_t **bsonNames,
     // printf("-- JADIOS DEBUG PRINT: count_names %d\n", *varCount);
     std::cout << "-- JADIOS DEBUG PRINT: count_names " << *varCount << std::endl;
 
-    // *names = (char **)g_slice_alloc(*count_names * sizeof(char *));
-    // *types = (int *)g_slice_alloc(*count_names * sizeof(int));
-    // bson_iter_init(&b_iter, bsonNames);
-
-    // for (unsigned int i = 0; i < *count_names; i++)
-    // {
-    //     if (!bson_iter_next(&b_iter))
-    //     {
-    //         printf("ERROR: count of names does not match \n");
-    //     }
-    //     (*names)[i] = g_strdup(bson_iter_key(&b_iter));
-    //     (*types)[i] = bson_iter_int32(&b_iter);
-    //     // printf("-- JADIOS DEBUG PRINT: get_all_var_names_from_kv DEBUG
-    //     PRINT:
-    //     // %s\n", (*names)[i]); printf("-- JADIOS DEBUG PRINT: types DEBUG
-    //     // PRINT: %d\n", (*types)[i]);
-    // }
-    // if (value_len > 0)
-    // {
-    //     bson_destroy(bson_names);
-    // }
     j_kv_unref(kv_object);
     j_batch_unref(batch);
    std::cout << "-- JADIOS DEBUG --- PRINT 1 " << std::endl;
