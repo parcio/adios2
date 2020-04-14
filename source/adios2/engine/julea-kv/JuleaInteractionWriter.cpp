3c927553978ea6ce4f736f73b24adbc532808160
Kira Duwe <duwe@informatik.uni-hamburg.de>
2020-04-14 11:05:52 +0200

    fixed stepmetadata writing

--- source/adios2/engine/julea-kv/JuleaInteractionWriter.cpp
+++ source/adios2/engine/julea-kv/JuleaInteractionWriter.cpp
@@ -182,6 +182,89 @@ void WriteVarMetadataToJuleaKV(Variable<T> &variable,
     std::cout << "stringMetadataKV " << stringMetadataKV << std::endl;
     std::cout << "varName " << variable.m_Name << std::endl;
 
+    // j_kv_get(kvVarMetadata, &metaDataBuf, &valueLen, batch);
+    // j_batch_execute(batch);
+
+    // std::cout << "valueLen: " << valueLen << std::endl;
+
+    char *buffer = NULL;
+
+    bool constantDims = variable.IsConstantDims();
+    size_t typeLen = sizeof(variable.m_Type.c_str());
+    const char *type = variable.m_Type.c_str();
+
+    size_t shapeSize = variable.m_Shape.size();
+    size_t shapeLen = shapeSize * sizeof(Dims[0]);
+
+    size_t startSize = variable.m_Start.size();
+    size_t startLen = startSize * sizeof(Dims[0]);
+
+    size_t countSize = variable.m_Count.size();
+    size_t countLen = countSize * sizeof(Dims[0]);
+
+    size_t numberSteps = currStep + 1;
+    size_t blocksLen = numberSteps * sizeof(size_t);
+    // calculating buffer size
+    size_t len = sizeof(bool) + typeLen + sizeof(size_t) + shapeLen +
+                 sizeof(size_t) + // shapeSize + size for counter
+                 startLen + sizeof(size_t) + countLen + sizeof(size_t) +
+                 blocksLen + sizeof(size_t);
+    std::cout << "len: " << len << std::endl;
+
+    buffer = (char *)g_slice_alloc(len);
+
+    // size_t *blocks = (size_t *)g_slice_alloc(numberSteps * sizeof(size_t));
+    size_t blocks[numberSteps];
+    for (uint i = 0; i < numberSteps; i++)
+    {
+        std::cout << "--- DEBUG ---" << std::endl;
+        // blocks[i] = variable.m_AvailableStepBlockIndexOffsets[i].at(0);
+        blocks[i] = variable.m_AvailableStepBlockIndexOffsets[i].size();
+        // blocks[i] = blockID + 1;
+        std::cout << "i: " << i << "  blocks: " << blocks[i] << std::endl;
+    }
+
+        std::cout << "--- DEBUG 1 ---" << std::endl;
+    memcpy(buffer, &constantDims, sizeof(bool));
+    buffer += sizeof(bool);
+    memcpy(buffer, &typeLen, sizeof(size_t));
+    buffer += sizeof(size_t);
+    memcpy(buffer, type, typeLen);
+    buffer += typeLen;
+    memcpy(buffer, &shapeSize, sizeof(size_t));
+    // memcpy(buffer, (void*) variable.m_Shape.size(), sizeof(size_t));
+    buffer += sizeof(size_t);
+    memcpy(buffer, variable.m_Shape.data(), shapeLen);
+    buffer += shapeLen;
+    memcpy(buffer, &startSize, sizeof(size_t));
+    buffer += sizeof(size_t);
+    memcpy(buffer, variable.m_Start.data(), startLen);
+    buffer += startLen;
+    memcpy(buffer, &countSize, sizeof(size_t));
+    buffer += sizeof(size_t);
+    memcpy(buffer, variable.m_Count.data(), countLen);
+    buffer += countLen;
+    memcpy(buffer, &numberSteps, sizeof(size_t));
+    buffer += sizeof(size_t);
+    memcpy(buffer, blocks, blocksLen);
+    // buffer += blocksLen;
+
+    // metaDataBuf = g_memdup(&buffer, len);
+    std::cout << "--- DEBUG 2 ---" << std::endl;
+    metaDataBuf = g_memdup(buffer, len);
+    std::cout << "--- DEBUG 3 ---" << std::endl;
+    // j_kv_put(kvVarMetadata, buffer, len, g_free, batch);
+    std::cout << "--- DEBUG 3 ---" << std::endl;
+    j_kv_put(kvVarMetadata, metaDataBuf, len, g_free, batch);
+    std::cout << "--- DEBUG 4 ---" << std::endl;
+    j_batch_execute(batch);
+    std::cout << "--- DEBUG 5 ---" << std::endl;
+
+    g_free(stringMetadataKV);
+    j_kv_unref(kvVarMetadata);
+    j_batch_unref(batch);
+    j_semantics_unref(semantics);
+
     // JuleaKVWriter::StepMetadata md =
     // g_slice_new(JuleaKVWriter::StepMetadata);
     // JuleaKVWriter::StepMetadata md;
@@ -216,10 +299,9 @@ void WriteVarMetadataToJuleaKV(Variable<T> &variable,
 
     // std::cout << "md->numberSteps: " << md.numberSteps << std::endl;
     // j_kv_get(kvVarMetadata, (gpointer *)&md, &valueLen, batch);
-    j_kv_get(kvVarMetadata, &metaDataBuf, &valueLen, batch);
-    j_batch_execute(batch);
 
-    std::cout << "valueLen: " << valueLen << std::endl;
+
+
     // if (valueLen == 0)
     // {
     //     std::cout << "--- DEBUG --- todo something here" << std::endl;
@@ -331,77 +413,6 @@ void WriteVarMetadataToJuleaKV(Variable<T> &variable,
     //     // j_batch_execute(batch);
     // }
 
-    char *buffer = NULL;
-
-    bool constantDims = variable.IsConstantDims();
-    size_t typeLen = sizeof(variable.m_Type.c_str());
-    const char *type = variable.m_Type.c_str();
-
-    size_t shapeSize = variable.m_Shape.size();
-    size_t shapeLen = shapeSize * sizeof(Dims[0]);
-
-    size_t startSize = variable.m_Start.size();
-    size_t startLen = startSize * sizeof(Dims[0]);
-
-    size_t countSize = variable.m_Count.size();
-    size_t countLen = countSize * sizeof(Dims[0]);
-
-    size_t numberSteps = currStep + 1;
-    size_t blocksLen = numberSteps * sizeof(size_t);
-    // calculating buffer size
-    size_t len = sizeof(bool) + typeLen + sizeof(size_t) + shapeLen +
-                 sizeof(size_t) + // shapeSize + size for counter
-                 startLen + sizeof(size_t) + countLen + sizeof(size_t) +
-                 blocksLen + sizeof(size_t);
-    std::cout << "len: " << len << std::endl;
-
-    buffer = (char *)g_slice_alloc(len);
-
-    // size_t *blocks = (size_t *)g_slice_alloc(numberSteps * sizeof(size_t));
-    size_t blocks[numberSteps];
-    for (uint i = 0; i < numberSteps; i++)
-    {
-        std::cout << "--- DEBUG ---" << std::endl;
-        // blocks[i] = variable.m_AvailableStepBlockIndexOffsets[i].at(0);
-        blocks[i] = variable.m_AvailableStepBlockIndexOffsets[i].size();
-        // blocks[i] = blockID + 1;
-        std::cout << "i: " << i << "  blocks: " << blocks[i] << std::endl;
-    }
-
-    memcpy(buffer, &constantDims, sizeof(bool));
-    buffer += sizeof(bool);
-    memcpy(buffer, &typeLen, sizeof(size_t));
-    buffer += sizeof(size_t);
-    memcpy(buffer, type, typeLen);
-    buffer += typeLen;
-    memcpy(buffer, &shapeSize, sizeof(size_t));
-    // memcpy(buffer, (void*) variable.m_Shape.size(), sizeof(size_t));
-    buffer += sizeof(size_t);
-    memcpy(buffer, variable.m_Shape.data(), shapeLen);
-    buffer += shapeLen;
-    memcpy(buffer, &startSize, sizeof(size_t));
-    buffer += sizeof(size_t);
-    memcpy(buffer, variable.m_Start.data(), startLen);
-    buffer += startLen;
-    memcpy(buffer, &countSize, sizeof(size_t));
-    buffer += sizeof(size_t);
-    memcpy(buffer, variable.m_Count.data(), countLen);
-    buffer += countLen;
-    memcpy(buffer, &numberSteps, sizeof(size_t));
-    buffer += sizeof(size_t);
-    memcpy(buffer, blocks, blocksLen);
-    buffer += blocksLen;
-
-    std::cout << "--- DEBUG 2 ---" << std::endl;
-    j_kv_put(kvVarMetadata, buffer, len, g_free, batch);
-    std::cout << "--- DEBUG 3 ---" << std::endl;
-    j_batch_execute(batch);
-    std::cout << "--- DEBUG 4 ---" << std::endl;
-
-    g_free(stringMetadataKV);
-    j_kv_unref(kvVarMetadata);
-    j_batch_unref(batch);
-    j_semantics_unref(semantics);
 }
 
 template <class T>
