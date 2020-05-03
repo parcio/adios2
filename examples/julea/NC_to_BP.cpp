083750728fac2fba45e29b9c6e82a09846b4f155
Kira Duwe <duwe@informatik.uni-hamburg.de>
2020-05-03 19:53:53 +0200

    clean up. format output

--- examples/julea/NC_to_BP.cpp
+++ examples/julea/NC_to_BP.cpp
@@ -2,39 +2,11 @@
 #include <iostream>
 #include <netcdf>
 #include <vector>
-// #include <glib.h>
-//
-// auto numberGroups = netCDF::NcGroup::getGroupCount(location);
-// auto groupMap = netCDF::NcGroup::getGroups();
-// auto varMap = netCDF::NcGroup::getVars(&location);
-
-// auto varMap1 = netCDF::NcGroup::getVars(location);
-// auto varMap2 = netCDF::NcGroup::getVars(dataFile);
-// auto varMap3 = netCDF::NcGroup::getVars(dataFile.Location);
-// auto varMap4 = netCDF::NcGroup::getVars(dataFile.Location());
-// auto varMap5 = netCDF::NcGroup::getVars(dataFile.Location::Current);
-
-// auto varMap1 = netCDF::NcGroup::getVars(location2);
-// auto varMap2 = netCDF::NcGroup::getVars(dataFile2);
-// auto varMap3 = netCDF::NcGroup::getVars(dataFile2.Location);
-// auto varMap4 = netCDF::NcGroup::getVars(dataFile2.Location());
-// auto varMap5 = netCDF::NcGroup::getVars(dataFile2.Location::All);
-// auto varMap6 = netCDF::NcGroup::getVars(netCDF::NcGroup::Location::All);
-// auto varMap7 = netCDF::NcGroup::getVars();
-// auto varMap8 = netCDF::NcGroup::getVars(5);
-// auto varMap9 = netCDF::NcGroup::getVars((netCDF::NcGroup::Location::All)5);
 
 void read(std::string engine, std::string fileName)
 {
-    std::cout << "fileName: " << fileName << std::endl;
+    std::cout << "\nfileName: " << fileName << std::endl;
     std::cout << "engine: " << engine << std::endl;
-    // read in nc file
-    // get all variable names
-
-    // for every name
-    // get data
-    // first dimension = time -> begin step .. end step
-    // Open the file.
 
     size_t varCount = 0;
     const size_t Nglobal = 2;
@@ -49,9 +21,10 @@ void read(std::string engine, std::string fileName)
 
     for (const auto &var : varMap)
     {
+        std::cout << "\n---------------------- variable: " << varCount
+                  << "----------------------" << std::endl;
         std::string name = var.first;
         netCDF::NcVar variable = var.second;
-        std::cout << "-- name:" << name << std::endl;
 
         netCDF::NcType type =
             variable
@@ -59,16 +32,11 @@ void read(std::string engine, std::string fileName)
         auto typeID = type.getId();
         auto typeName = type.getName();
 
-        std::cout << "-- type:" << type.getName() << std::endl;
-        std::cout << "--- type ID: " << typeID << std::endl;
+        std::cout << "" << name << " - " << type.getName() << ":" << std::endl;
 
         // auto adiosVarName = g_strdup_printf("Variable_%lu", varCount);
         auto adiosVarName = "variable_" + std::to_string(varCount);
-        std::cout << "adiosVarName: " << adiosVarName << std::endl;
-
-        const std::string varType = "double";
-        // adios2::Variable<varType> var0 = io.DefineVariable<varType>("v0",
-        // {},{},{Nglobal});
+        std::cout << " " << adiosVarName << std::endl;
 
         if (typeID == NC_BYTE)
         {
@@ -94,27 +62,27 @@ void read(std::string engine, std::string fileName)
         }
         else if (typeID == NC_UBYTE)
         {
-                // io.DefineVariable<double>(adiosVarName, {}, {}, {Nglobal});
+            // io.DefineVariable<double>(adiosVarName, {}, {}, {Nglobal});
         }
         else if (typeID == NC_USHORT)
         {
-                // io.DefineVariable<double>(adiosVarName, {}, {}, {Nglobal});
+            // io.DefineVariable<double>(adiosVarName, {}, {}, {Nglobal});
         }
         else if (typeID == NC_UINT)
         {
-                io.DefineVariable<double>(adiosVarName, {}, {}, {Nglobal});
+            io.DefineVariable<double>(adiosVarName, {}, {}, {Nglobal});
         }
         else if (typeID == NC_INT64)
         {
-                io.DefineVariable<int64_t>(adiosVarName, {}, {}, {Nglobal});
+            io.DefineVariable<int64_t>(adiosVarName, {}, {}, {Nglobal});
         }
         else if (typeID == NC_UINT64)
         {
-                io.DefineVariable<uint64_t>(adiosVarName, {}, {}, {Nglobal});
+            io.DefineVariable<uint64_t>(adiosVarName, {}, {}, {Nglobal});
         }
         else if (typeID == NC_STRING)
         {
-                io.DefineVariable<std::string>(adiosVarName, {}, {}, {Nglobal});
+            io.DefineVariable<std::string>(adiosVarName, {}, {}, {Nglobal});
         }
         else if (typeID == NC_VLEN)
         {
@@ -129,35 +97,10 @@ void read(std::string engine, std::string fileName)
         {
         }
 
-        // if (typeID = )
-        // if(typeID = 3)
-        // {
-        // 		auto test = io.DefineVariable<int32_t>(adiosVarName,
-        // {},{},{Nglobal});
-
-        // }
-        // else if(typeID = 4)
-        // {
-        // 		auto test = io.DefineVariable<float>(adiosVarName,
-        // {},{},{Nglobal});
-
-        // }
-        // switch(typeID)
-        // {
-        // 	case NC_BYTE:
-        // 	std::cout << "test " << std::endl;
-        // 		break;
-        // 	case NC_CHAR:
-        // 		// auto var = io.DefineVariable<char>(adiosVarName,
-        // {},{},{Nglobal}); 	std::cout << "test2 " << std::endl; 		break; 	case
-        // NC_SHORT: 		auto var = io.DefineVariable<int8_t>(adiosVarName,
-        // {},{},{Nglobal}); 		break; 	case NC_INT: 		auto var =
-        // io.DefineVariable<int32_t>(adiosVarName, {},{},{Nglobal}); 		break;
-        // 	case NC_FLOAT:
-        // 		auto var = io.DefineVariable<float>(adiosVarName,
-        // {},{},{Nglobal}); 		break;
-        // }
-
+        //inquire variable
+        //get data
+        //first dimension = time -> begin step .. end step
+        //put variable
         ++varCount;
     }
 }
@@ -165,8 +108,7 @@ void read(std::string engine, std::string fileName)
 int main(int argc, char *argv[])
 {
     int rank = 0;
-    std::cout << "... SimpleStepTest ... " << std::endl;
-    std::cout << "... Only one process ... " << std::endl;
+    std::cout << "... Convert nc file to bp/jv/jb ... " << std::endl;
 
     try
     {
