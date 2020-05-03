4357dc00d403ed8f42138094a0e0343bba4cfbf5
Kira Duwe <duwe@informatik.uni-hamburg.de>
2020-05-03 19:41:02 +0200

    define variables

--- examples/julea/NC_to_BP.cpp
+++ examples/julea/NC_to_BP.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <adios2.h>
 #include <netcdf>
-
+// #include <glib.h>
 //
 // auto numberGroups = netCDF::NcGroup::getGroupCount(location);
      // auto groupMap = netCDF::NcGroup::getGroups();
@@ -27,6 +27,7 @@
 void read(std::string engine, std::string fileName)
 {
 	std::cout << "fileName: " << fileName << std::endl;
+	std::cout << "engine: " << engine << std::endl;
 	//read in nc file
 	//get all variable names
 
@@ -35,31 +36,76 @@ void read(std::string engine, std::string fileName)
 		//first dimension = time -> begin step .. end step
 	// Open the file.
 
+
+
+     size_t varCount = 0;
+	const size_t Nglobal = 2;
+	adios2::ADIOS adios(adios2::DebugON);
+    adios2::IO io = adios.DeclareIO("Output");
+    io.SetEngine(engine);
+
      netCDF::NcFile dataFile;
      dataFile.open(fileName, netCDF::NcFile::read);
 
      auto varMap = dataFile.getVars();
 
-     size_t varCount = 0;
      for(const auto &var : varMap)
      {
      	std::string name = var.first;
      	netCDF::NcVar variable = var.second;
      	std::cout << "-- name:" << name << std::endl;
 
-     	netCDF::NcType type = dataFile.getType(name,netCDF::NcGroup::Location::All);
+     	netCDF::NcType type = variable.getType();//dataFile.getType(name,netCDF::NcGroup::Location::All);
      	auto typeID = type.getId();
+     	auto typeName = type.getName();
 
-     	netCDF::NcType testType(3);
-     	auto typeID2 = testType.getId();
-     	// auto typeName = type.getName();
-     	// std::cout << "-- type:" << type.getName() << std::endl;
+     	std::cout << "-- type:" << type.getName() << std::endl;
      	std::cout << "--- type ID: " << typeID << std::endl;
-     	std::cout << "--- type ID2: " << typeID2 << std::endl;
+
+     	// auto adiosVarName = g_strdup_printf("Variable_%lu", varCount);
+     	auto adiosVarName = "variable_" + std::to_string(varCount);
+
+     	// size_t size = sizeof("variable_") + sizeof(varCount) + 1;
+     	// std::unique_ptr<char[]> buf( new char[ size ] );
+     	// snprintf( buf.get(), size , "variable_%lu", varCount);
+     	// std::string adiosVarName (buf.get(), buf.get() + size -1);
+     	std::cout << "adiosVarName: " << adiosVarName << std::endl;
+
+     	const std::string varType = "double";
+     	// adios2::Variable<varType> var0 = io.DefineVariable<varType>("v0", {},{},{Nglobal});
+
+     	if(typeID = 3)
+     	{
+     			auto test = io.DefineVariable<int32_t>(adiosVarName, {},{},{Nglobal});
+
+     	}
+     	else if(typeID = 4)
+     	{
+     			auto test = io.DefineVariable<float>(adiosVarName, {},{},{Nglobal});
+
+     	}
+     	switch(typeID)
+     	{
+     		case NC_BYTE:
+     		std::cout << "test " << std::endl;
+     			break;
+     		case NC_CHAR:
+     			// auto var = io.DefineVariable<char>(adiosVarName, {},{},{Nglobal});
+     		std::cout << "test2 " << std::endl;
+     			break;
+     		case NC_SHORT:
+     			auto var = io.DefineVariable<int8_t>(adiosVarName, {},{},{Nglobal});
+     			break;
+     		case NC_INT:
+     			auto var = io.DefineVariable<int32_t>(adiosVarName, {},{},{Nglobal});
+     			break;
+     		case NC_FLOAT:
+     			auto var = io.DefineVariable<float>(adiosVarName, {},{},{Nglobal});
+     			break;
+     	}
 
      	++varCount;
      }
-
 }
 
 int main(int argc, char *argv[])
@@ -71,8 +117,10 @@ int main(int argc, char *argv[])
     try
     {
     	//example file from "https://www.unidata.ucar.edu/software/netcdf/examples/files.html"
-    	read("julea-db", "sresa1b_ncar_ccsm3-example.nc");
-    	read("julea-db", "_grib2netcdf-webmars-public-svc-blue-004-6fe5cac1a363ec1525f54343b6cc9fd8-ICkLWm.nc");
+    	// read("julea-db", "sresa1b_ncar_ccsm3-example.nc");
+    	// read("julea-db", "_grib2netcdf-webmars-public-svc-blue-004-6fe5cac1a363ec1525f54343b6cc9fd8-ICkLWm.nc");
+    	read("bp3", "sresa1b_ncar_ccsm3-example.nc");
+    	read("bp3", "_grib2netcdf-webmars-public-svc-blue-004-6fe5cac1a363ec1525f54343b6cc9fd8-ICkLWm.nc");
     }
     catch (std::invalid_argument &e)
     {
