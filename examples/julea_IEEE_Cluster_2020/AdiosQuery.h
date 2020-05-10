f2bda391cb0b97f3410c81a295de1097ce42d9d4
Kira Duwe <duwe@informatik.uni-hamburg.de>
2020-05-10 19:14:03 +0200

    cleanup

--- examples/julea_IEEE_Cluster_2020/AdiosQuery.h
+++
@@ -1,6 +0,0 @@
-#ifndef ADIOS2_QUERY_H_
-#define ADIOS2_QUERY_H_
-
-void AdiosReadMinMax(std::string fileName, std::string variableName);
-
-#endif /* ADIOS2_QUERY_H_ */
--- examples/julea_IEEE_Cluster_2020/JuleaQuery.h
+++
@@ -1,6 +0,0 @@
-#ifndef JULEA_QUERY_H_
-#define JULEA_QUERY_H_
-
-void JuleaReadMinMax(std::string fileName, std::string variableName);
-
-#endif /* JULEA_QUERY_H_ */
