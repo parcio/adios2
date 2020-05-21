dba0df27680501f8cd8b6e4caeb3d6206e1cdb23
Kira Duwe <0duwe@informatik.uni-hamburg.de>
2020-05-21 21:48:22 +0200

    fixed output file name

--- examples/julea_IEEE_Cluster_2020/NCtoBP.sh
+++ examples/julea_IEEE_Cluster_2020/NCtoBP.sh
@@ -8,18 +8,23 @@ declare -A result
 ending=(["bp3"]="bp" ["bp4"]="bp" ["julea-kv"]="jv" ["julea-db"]="jb")
 
 # echo -e "#interlines\t iterations\t io-time[s] ${engines[*]}"
-echo -e "#engine\t io-time[s] ${engines[*]} \t file"
+echo -e "engine\t io-time[ms] \t file"
 
 for file in /home/duwe/ieee_cluster_2020_adios2/ecmwf-data/*.nc
 do
 	for engine in $engines
 	do
-		outfile=/tmp/$engine-Files/$file.${ending[$engine]}
+		#outfile=/tmp/$engine-Files/$file.${ending[$engine]}
+		filename=$(basename -- "$file")
+		extension="${filename##*.}"
+		filename="${filename%.*}"
+		outfile=/tmp/$engine-Files/$filename.${ending[$engine]}
+		#echo -e "$filename"
 		path=julea-adios2/build/bin/NC_TO_BP
 		# 	read fileCount often varCount many vars from engine-Files
 		result[$engine]="$(./$path -d $file -f $outfile -n $engine -t )\t"
 	done
-	echo -e "$engine\t  ${result[*]} \t $file"
+	echo -e "$engine\t  ${result[*]} \t $filename"
 	# echo -e "$interline\t $iteration\t ${result[*]}"
 done
 
