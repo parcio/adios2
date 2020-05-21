ffa587b8ec3b1eb5698a29f5a60e74a08edd1a72
Kira Duwe <duwe@informatik.uni-hamburg.de>
2020-05-21 17:25:37 +0200

    cluster skripts

---
+++ examples/julea_IEEE_Cluster_2020/NCtoBP.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+
+declare -A ending
+declare -A result
+
+ending=(["bp3"]="bp" ["bp4"]="bp" ["julea-kv"]="jv" ["julea-db"]="jb")
+
+# echo -e "#interlines\t iterations\t io-time[s] ${engines[*]}"
+echo -e "#files\t vars\t io-time[s] ${engines[*]}"
+
+for file in /home/duwe/ieee_cluster_2020_adios2/ecmwf-data/*.nc
+do
+	for engine in $engines
+	do
+		outfile=/tmp/$engine-Files/$file.${ending[$engine]}
+		path=/home/duwe/ieee_cluster_2020_adios2/julea-adios2/build/bin/NC_TO_BP
+		# 	read fileCount often varCount many vars from engine-Files
+		result[$engine]="$(./path -d $file -f $outfile -n $engine -t )\t"
+	done
+	echo -e "$fileCount\t $varCount\t ${result[*]}"
+	# echo -e "$interline\t $iteration\t ${result[*]}"
+done
+
--- examples/julea_IEEE_Cluster_2020/benchmarkAdiosRead.sh
+++ examples/julea_IEEE_Cluster_2020/benchmarkAdiosRead.sh
@@ -1,29 +1,37 @@
 #! /usr/bin/env bash
 
-iterations="1 2 4 8"
-#interlines="0 64 512 1024 2048 4096"
-interlines="4096"
+#100= more than in any file -> all
+vars="1 4 8 16 32 100"
+files="1 2 4 8 16 32 64 128 256 512"
 engines="bp3 bp4 julea-kv julea-db"
-declare -A ending 
+
+declare -A ending
 declare -A result
+
 ending=(["bp3"]="bp" ["bp4"]="bp" ["julea-kv"]="jv" ["julea-db"]="jb")
 
-echo -e "#interlines\t iterations\t io-time[s] ${engines[*]}"
-for interline in $interlines
+# echo -e "#interlines\t iterations\t io-time[s] ${engines[*]}"
+echo -e "#files\t vars\t io-time[s] ${engines[*]}"
+# for interline in $interlines
+for fileCount in $files
 do
-	for iteration in $iterations
+	for varCount in $vars
+	# for iteration in $iterations
 	do
 		for engine in $engines
 		do
-			outfile=/tmp/bpFiles/$engine-$interline-$iteration.${ending[$engine]}
+			outfile=/tmp/AdiosReadBenchmark/$engine-$fileCount-$varCount.${ending[$engine]}
 			#outfile=$engine-$interline-$iteration.${ending[$engine]}
 			# 100 nach $iteration für checkpoints in jeder
 			# iteration (100%)
-			result[$engine]="$(./partdiff-par \
-				1 2 $interline 1 2 $iteration 100\
-				$outfile $engine | grep I/O | tr -d ' ' | \
-				tr -d 's' | cut -d : -f 2)\t"
+			# result[$engine]="$(./partdiff-par \
+			# 	1 2 $interline 1 2 $iteration 100\
+			# 	$outfile $engine | grep I/O | tr -d ' ' | \
+			# 	tr -d 's' | cut -d : -f 2)\t"
+			# 	read fileCount often varCount many vars from engine-Files
+			result[$engine]="$(./bin/BENCHMARK -d $engine-Files -c $fileCount -p $varCount -n $engine -s2 )\t"
 		done
-		echo -e "$interline\t $iteration\t ${result[*]}"
+		echo -e "$fileCount\t $varCount\t ${result[*]}"
+		# echo -e "$interline\t $iteration\t ${result[*]}"
 	done
 done
