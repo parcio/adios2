8b0bb390f8f5a83abd37cc287f54a380ce14160c
Kira Duwe <kira.duwe@ovgu.de>
2021-12-13 20:21:38 +0100

    removing unused header. adding new class to cmake

--- source/adios2/engine/julea-db/JuleaDBWriter.h
+++ source/adios2/engine/julea-db/JuleaDBWriter.h
@@ -14,7 +14,6 @@
 #include "adios2/core/Engine.h"
 // #include "adios2/toolkit/format/bp3/BP3.h" //BP3Serializer
 #include "adios2/toolkit/format/bp/bp3/BP3Serializer.h"
-#include "adios2/toolkit/interop/julea/JuleaSerializer.h"
 #include "adios2/toolkit/transportman/TransportMan.h" //transport::TransportsMan
 
 #include <complex.h>
