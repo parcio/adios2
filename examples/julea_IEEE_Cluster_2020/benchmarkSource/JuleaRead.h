8540f7a2f95e7adc8a7f7e5a05315421115606f1
Kira Duwe <kira.duwe@ovgu.de>
2022-11-03 17:52:14 +0100

    updated cmakelists. removed code for IEEE cluster measurements. nc2bp will be part of utils in the future.

--- examples/julea_IEEE_Cluster_2020/benchmarkSource/JuleaRead.h
+++
@@ -1,8 +0,0 @@
-#ifndef JULEA_READ_H_
-#define JULEA_READ_H_
-
-void JuleaReadMinMax(std::string fileName, std::string variableName);
-void JuleaRead(std::string engineName, std::string directory, size_t fileCount, uint32_t percentageVarsToRead);
-
-
-#endif /* JULEA_READ_H_ */
--- examples/julea_IEEE_Cluster_2020/benchmarkSource/NetCDFRead.h
+++
@@ -1,6 +0,0 @@
-#ifndef NETCDF_READ_H
-#define NETCDF_READ_H
-#include <iostream>
-void NCReadFile(std::string engine, std::string ncFileName, std::string adiosFileName,
-          bool printDimensions, bool printVariable, bool needsTransform);
-#endif /* NETCDF_READ_H */
